3df6e0bb9e4071975b672e7a75fa2f7c6d3f0226
Zots Bohdan <bzots@ukr.net>
2021-11-06 14:45:36 +0300

    test

--- ts-CORE/6-generics/example-5.ts
+++ ts-CORE/6-generics/example-5.ts
@@ -1,6 +1,6 @@
 {
     const readLength = function <T>(arg: T): T {
-        // console.log(arg.length);  // Error: T doesn't have .length
+        /*  console.log(arg.length) */;  // Error: T doesn't have .length
         return arg;
     };
 }
